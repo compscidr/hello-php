f1bb7b837fb7a3c01ac59d555f587c4530c270b6
Jason Ernst <ernstjason1@gmail.com>
2024-01-17 14:19:06 -0800

    adds uses and covers

--- tests/GreetingsTest.php
+++ tests/GreetingsTest.php
@@ -3,6 +3,10 @@
 use HelloPhp\Greetings;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @uses \HelloPhp\Greetings
+ * @covers \HelloPhp\Greetings
+ */
 final class GreetingsTest extends TestCase {
     public function testHello(): void {
         $expectedOutput = "Hello World";
